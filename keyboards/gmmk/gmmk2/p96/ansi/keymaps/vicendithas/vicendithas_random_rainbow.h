809f6a3b308a1da2441c82ac571317e19ffca3ad
Vicendithas <vincent.killion@gmail.com>
2025-01-14 23:32:24 -0500

    Update Animation

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/vicendithas/vicendithas_random_rainbow.h
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/vicendithas/vicendithas_random_rainbow.h
@@ -2,14 +2,21 @@ RGB_MATRIX_EFFECT(vicendithas_random_rainbow)
 
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static hsv_t vicendithas_random_rainbow_math(hsv_t hsv, uint8_t i, uint8_t time) {
-	hsv.h = rand() % 256;
-
-	return hsv;
-}
-
 static bool vicendithas_random_rainbow(effect_params_t* params) {
-	return effect_runner_i(params, &vicendithas_random_rainbow_math);
+	RGB_MATRIX_USE_LIMITS(led_min, led_max);
+	
+	static fast_timer_t timer = 0;
+	
+	if ((params->iter == 0) && (timer_elapsed_fast(timer) > (320 - rgb_matrix_config.speed))) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+			hsv_t hsv = {random8(), rgb_matrix_config.hsv.s, rgb_matrix_config.hsv.v};
+			rgb_t rgb = rgb_matrix_hsv_to_rgb(hsv);
+			rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+		}
+		timer = timer_read_fast();
+    }
+	
+	return rgb_matrix_check_finished_leds(led_max);
 }
 
 #endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
